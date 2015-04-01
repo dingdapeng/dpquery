a6b91572ac7f9e3db8427ce8adf28be4e08978c5
dapeng <569305586@qq.com>
2015-04-01 14:44:41 +0800

    新增操作cookie方法

--- DpQuery.js
+++ DpQuery.js
@@ -292,8 +292,46 @@
     }
 
      
-     
 
+  DpQuery.setCookie= function (name, value, time) {
+        var strsec = this.getsec(time);
+        var exp = new Date();
+        exp.setTime(exp.getTime() + strsec * 1);
+        document.cookie = name + "=" + escape(value) + ";expires=" + exp.toGMTString();
+    }
+  
+  DpQuery.getsec= function(str) {
+        
+        var str1 = str.substring(1, str.length) * 1;
+        var str2 = str.substring(0, 1);
+        if (str2 == "s") {
+            return str1 * 1000;
+        }
+        else if (str2 == "h") {
+            return str1 * 60 * 60 * 1000;
+        }
+        else if (str2 == "d") {
+            return str1 * 24 * 60 * 60 * 1000;
+        }
+    }
+    
+  DpQuery.getCookie= function (name) {
+        var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+
+        if (arr = document.cookie.match(reg))
+
+            return unescape(arr[2]);
+        else
+            return null;
+    }
+ 
+  	
+  	 for(var i in obj){
+  	     
+  	 	this[i]=obj[i];
+  	 }
+  };
+  
     window.DpQuery = window.$ = DpQuery;
 
 
